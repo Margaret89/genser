f70e20e70510ee4d966e048c39108c13ba90ff2e
DESKTOP-1SDMCHM\Margarita <match14@yandex.ru>
2019-05-05 17:53:13 +0300

    footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
 
-	//---------- Маска для телефона -------------
-	// $.mask.definitions['~'] = "[+-]";
-	// $("#phone").mask("(999) 999-9999");
+	// ---------- Маска для телефона -------------
+	$.mask.definitions['~'] = "[+-]";
+	$(".js-phone").mask("+7 (999) 999-9999");
+
+	// Вывод сообщения об успешной отправке в попапе
+	$('.js-valid-form').each(function(){
+		$(this).on('submit',function(e){
+			$.fancybox.close();
+			$.fancybox.open({
+				src  : '#msg-success',
+				type : 'inline',
+				opts : {
+					
+				}
+			});
+			$(this)[0].reset();
+			e.preventDefault();
+		});
+	});
 
 	// Вызов функции подгрузки изображений
 	// loadBigImg();
