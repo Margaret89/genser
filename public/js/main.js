34e23f4ac7c5fe0d1d616685b8880c11ae24904a
DESKTOP-1SDMCHM\Margarita <match14@yandex.ru>
2019-05-05 15:20:19 +0300

    структура файлов и header

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+	//---------- Маска для телефона -------------
+	// $.mask.definitions['~'] = "[+-]";
+	// $("#phone").mask("(999) 999-9999");
+
+	// Вызов функции подгрузки изображений
+	// loadBigImg();
+	// loadBigBacground();
+
+});
+
+// Загрузка больших изображений
+// function loadBigImg() {
+// 	var $imgDefer = $('[data-src]');
+
+// 	$imgDefer.each(function(indx, element){
+// 		var urlImgBig = $(this).attr("data-src");
+// 		$(this).attr("src", urlImgBig);
+// 	});
+// }
+
+// function loadBigBacground() {
+// 	var $imgDefer = $('[data-background]');
+
+// 	$imgDefer.each(function(indx, element){
+// 		var urlBackgroundBig = $(this).attr("data-background");
+// 		$(this).css("background-image", "url("+ urlBackgroundBig +")");
+// 	});
+// }
